991ae437b17a1d4a7367c2df038c6b05d11446d3
Gord Bolli <gord@bolli.ca>
2022-08-12 13:00:48 -0700

    Set up DB and packages for use

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+const PORT = 3000;
+require('dotenv').config();
+
+// Mongo DB setup
+let db,
+    db_connectionString = process.env.DB_STRING,
+    db_name = 'todo';
+
+MongoClient.connect(db_connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${db_name} database`);
+        db = client.db(db_name);
+    })
+
+// Set & Use
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Routes
+
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
