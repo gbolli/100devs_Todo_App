361ba96e902a07595672167652872882328aa395
Gord Bolli <gord@bolli.ca>
2022-09-08 12:13:19 -0700

    Add logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./config/database')
+const logger = require('morgan')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
@@ -13,6 +14,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(logger('dev'))
 
 // Routes
 
@@ -24,3 +26,4 @@ app.listen(process.env.PORT, ()=>{
 })
 
 // TODO:  add authentication
+
