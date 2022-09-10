c7d511030572b10158ce230f5fe426e8f979aafd
Gord Bolli <gord@bolli.ca>
2022-09-09 17:03:58 -0700

    Add ensureAuth middleware

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
 const todosController = require('../controllers/todos')
+const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', todosController.getTodos)
+router.get('/', ensureAuth, todosController.getTodos)
 router.post('/createTodo', todosController.createTodo)
 router.put('/markComplete', todosController.markComplete)
 router.put('/markIncomplete', todosController.markIncomplete)
